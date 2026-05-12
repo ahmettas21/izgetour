c5b06d0a84ba28ef420a6e95911d8aebfe796d90
userclawturk <userclawturk@users.noreply.github.com>
2026-05-12 19:11:56 +0000

    fix: _app.tsx import path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { SupabaseProvider } from '../../components/auth/SupabaseProvider';
+import { SupabaseProvider } from '../components/auth/SupabaseProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
